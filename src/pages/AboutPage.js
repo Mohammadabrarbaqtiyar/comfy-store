f4eb7305c091bd91b62e109af0aa96ded4345fc1
Mohammad Abrar <mjece39@gmail.com>
2022-09-07 02:14:04 +0530

    added text to landing page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,32 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
   return (
     <main>
-      <PageHero title='about' />
-      <Wrapper className='page section section-center'>
-        <img src={aboutImg} alt='nice desk' />
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
         <article>
-          <div className='title'>
+          <div className="title">
             <h2>our story</h2>
-            <div className='underline'></div>
+            <div className="underline"></div>
           </div>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-            quaerat, modi doloremque necessitatibus eum dolor nesciunt delectus,
-            voluptate blanditiis, obcaecati beatae ab aut ipsa consequuntur
-            tempora cumque. Ut quo enim vero odio minus nostrum eveniet,
-            doloribus veritatis dolorem unde ipsum, voluptatibus totam.
-            Explicabo, quas libero! Laborum incidunt minima consequatur ratione?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ad
+            fugiat vero rem quisquam? Porro, repellat ad ducimus tempore
+            dolorum, quae tenetur, repudiandae quos adipisci nemo veritatis
+            natus nisi officiis.
           </p>
         </article>
       </Wrapper>
     </main>
-  )
-}
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -54,5 +52,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
